7b5b21ede00f0e4e849e59581a40f201f7e4263e
WozZz <vyphamhung10@gmail.com>
2019-04-23 00:57:56 +0700

    123

--- bakerydemo/base/models.py
+++ bakerydemo/base/models.py
@@ -176,25 +176,25 @@ class HomePage(Page):
     )
 
     # Promo section of the HomePage
-    promo_image = models.ForeignKey(
-        'wagtailimages.Image',
-        null=True,
-        blank=True,
-        on_delete=models.SET_NULL,
-        related_name='+',
-        help_text='Promo image'
-    )
-    promo_title = models.CharField(
-        null=True,
-        blank=True,
-        max_length=255,
-        help_text='Title to display above the promo copy'
-    )
-    promo_text = RichTextField(
-        null=True,
-        blank=True,
-        help_text='Write some promotional copy'
-    )
+    # promo_image = models.ForeignKey(
+    #     'wagtailimages.Image',
+    #     null=True,
+    #     blank=True,
+    #     on_delete=models.SET_NULL,
+    #     related_name='+',
+    #     help_text='Promo image'
+    # )
+    # promo_title = models.CharField(
+    #     null=True,
+    #     blank=True,
+    #     max_length=255,
+    #     help_text='Title to display above the promo copy'
+    # )
+    # promo_text = RichTextField(
+    #     null=True,
+    #     blank=True,
+    #     help_text='Write some promotional copy'
+    # )
 
     # Featured sections on the HomePage
     # You will see on templates/base/home_page.html that these are treated
@@ -261,11 +261,6 @@ class HomePage(Page):
                 PageChooserPanel('hero_cta_link'),
                 ])
             ], heading="Hero section"),
-        MultiFieldPanel([
-            ImageChooserPanel('promo_image'),
-            FieldPanel('promo_title'),
-            FieldPanel('promo_text'),
-        ], heading="Promo section"),
         StreamFieldPanel('body'),
         MultiFieldPanel([
             MultiFieldPanel([
